3734a52f4b0d65929b9655309d9dfb51cc01dcaf
zzangyoon0710@gmail.com <zzangyoon710@gmail.com>
2023-03-20 16:54:15 +0900

    프로젝트 생성

---
+++ src/test/java/com/example/stockPriceInfo/StockPriceInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stockPriceInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockPriceInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
